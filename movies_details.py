91a1d010bcc522994057d217b1846f8adefa85e5
soumitasardar <45121616+soumitasardar@users.noreply.github.com>
2019-03-08 10:59:48 +0530

    Update movies_details.py

--- movies_details.py
+++ movies_details.py
@@ -1,6 +1,9 @@
 import media
 import fresh_tomatoes
 
+'''
+Below are the list of instance created for class Movie
+'''
 avatar = media.Movie("Avatar",
 					"https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
 					"On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully (Sam Worthington), a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na'vi woman (Zoe Saldana). As a bond with her grows, he is drawn into a battle for the survival of her world.",
@@ -32,10 +35,9 @@ the_martian = media.Movie("The Martian",
 					"https://www.youtube.com/watch?v=Ue4PCI0NamI")
 
 
-#frozen.show_trailer()
-#avatar.show_poster()
-#print (lagaan.storyline)
 #------------------------------------------------------------------------------------------------------------------
+# To display the list of movies in a website with the trailer, we have used fresh_tomatoes.py file
+
 movie_list = [avatar,ratatouille,lagaan,the_martian,frozen,interstellar]
 website = fresh_tomatoes.open_movies_page(movie_list)
 print website
