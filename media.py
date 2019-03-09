9e7277b50969e447a9d6b2932b7d8a781dff72e6
soumitasardar <45121616+soumitasardar@users.noreply.github.com>
2019-03-09 22:12:30 +0530

    Update media.py
    pep-8 changes

--- media.py
+++ media.py
@@ -1,20 +1,21 @@
 import webbrowser
 
+
 class Movie():
-	'''
-	Created a structure in order to store the movie details
-	'''
-	def __init__(self,movie_title,movie_poster,movie_storyline,movie_trailer_url):   #self is used for the instance that we are going to create
-		self.title = movie_title
-		self.poster_image_url = movie_poster
-		self.storyline = movie_storyline
-		self.trailer_youtube_url = movie_trailer_url
+    def __init__(self, movie_title, movie_poster, movie_storyline,
+                 movie_trailer_url):
+        '''
+        Created a structure in order to store the movie details
+        '''
+        self.title = movie_title
+        self.poster_image_url = movie_poster
+        self.storyline = movie_storyline
+        self.trailer_youtube_url = movie_trailer_url
 
 
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+def show_trailer(self):
+    webbrowser.open(self.trailer_youtube_url)
 
 
-	def show_poster(self):
-		return webbrowser.open(self.poster)
-		
+def show_poster(self):
+    return webbrowser.open(self.poster)
