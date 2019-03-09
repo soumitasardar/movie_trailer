1b5b3546a2235f7fc7146afe1321b5aa30ebe6b5
soumitasardar <45121616+soumitasardar@users.noreply.github.com>
2019-03-09 22:37:10 +0530

    Update media.py
    updated comments

--- media.py
+++ media.py
@@ -4,8 +4,13 @@ import webbrowser
 class Movie():
     def __init__(self, movie_title, movie_poster, movie_storyline,
                  movie_trailer_url):
-        '''
-        Created a structure in order to store the movie details
+        '''Created a structure in order to store the movie details.
+        self :- examples or instancces of this class
+        This structure takes 4 string type parameters
+        movie_title :- Name of the movie
+        movie_poster :- image for the movie
+        moview_storyline :- Movie description
+        movie_trailer :- its a url of youtube video
         '''
         self.title = movie_title
         self.poster_image_url = movie_poster
